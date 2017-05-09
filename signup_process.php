ffc3550c4cd3448688f36569e632b3d9672f637c
birender dahiya <bd248@afsconnect1.njit.edu>
2017-05-08 21:17:42 -0400

    process

--- signup_process.php
+++ signup_process.php
@@ -17,7 +17,7 @@
       
 
 
-<div class="container" style="width:25%;position: relative;left: -10%;background: #F08080;padding: 8px 10px 8px 12px;color: #000000;border-radius: 7px;">
+<div class="container" style="width:45%;position: relative;left: -10%;background: #F08080;padding: 8px 10px 8px 12px;color: #000000;border-radius: 7px;">
   <h2>User Profile</h2>
   <form method="post" action="createaccount.php">
     <div class="form-group">
