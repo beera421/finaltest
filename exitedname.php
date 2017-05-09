3fca469247412942ee216ddba9cde490fb232112
birender dahiya <bd248@afsconnect1.njit.edu>
2017-05-08 21:19:30 -0400

    exitedname

--- exitedname.php
+++ exitedname.php
@@ -5,7 +5,7 @@
 	<div id="about" class="container-fluid">
 	  <div class="row">
 	    <div class="col-sm-8">
-	      <h4>This email already exists! choose another one</h4>
+	      <h4>This email already exists! please choose another one</h4>
 	      <br><a href="signup_process.php"> Go back to sign up page</a>
 	      <br>
 	    </div>
